ace60d07a2895d9af1bd799ef47f91f3a061b0ba
SeverusLin <517703019@qq.com>
2020-10-30 19:06:42 +0800

    RTest

--- REG.py
+++ REG.py
@@ -9,10 +9,10 @@ class MLR:
     def fit(self):
         if self.intercept:
             ones=np.ones(self.X.shape[0])
-            X=np.c_[ones,self.X]
+            A=np.c_[ones,self.X]
         else:
-            X=self.X
-        self.a=np.linalg.inv(X.T@X)@(X.T@self.Y)
+            A=self.X
+        self.a=np.linalg.inv(A.T@A)@(A.T@self.Y)
     def predict(self,Xnew):
         if self.intercept:
             ones=np.ones(Xnew.shape[0])
@@ -24,8 +24,19 @@ class MLR:
     def FTest(self,alpha):
         yHat=self.predict(self.X)
         Qe=((self.Y-yHat)**2).sum()
-        yAver=self.Y.mean()
+        yAver=np.mean(self.Y)
         U=((yHat-yAver)**2).sum()
         Fvalue=U/(Qe/(self.X.shape[0]-2))
         Falpha=f.isf(alpha,1,self.X.shape[0]-2)
         return Fvalue,Falpha,Fvalue>Falpha
+    def RTest(self,alpha):
+        Falpha=f.isf(alpha,1,self.X.shape[0]-2)
+        Ralpha=np.sqrt(1/(1+(self.X.shape[0]-2)/Falpha))
+        xAver=np.mean(self.X)
+        yAver=np.mean(self.Y)
+        Rup1=self.X-xAver
+        Rup2=self.Y-yAver
+        Rup=(Rup1*Rup2).sum()
+        Rdown=np.sqrt(((self.X-xAver)**2).sum()*((self.Y-yAver)**2).sum())
+        R=abs(Rup/Rdown)
+        return R,Ralpha,R>Ralpha
